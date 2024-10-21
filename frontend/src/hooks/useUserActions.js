32d2e962b4b774286557d52724623c563b494a6a
diego072117 <parracalderond9@gmail.com>
2024-10-20 21:30:43 -0500

    feat(UI): user interface login.

---
+++ frontend/src/hooks/useUserActions.js
@@ -0,0 +1,27 @@
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import {
+  loginUserAsync,
+  logout,
+  registerUserAsync,
+} from "../store/users/slice";
+
+export const useUserActions = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const NewUser = (userData) => {
+    dispatch(registerUserAsync(userData));
+  };
+
+  const LoginUser = async (userData) => {
+    return dispatch(loginUserAsync(userData));
+  };
+
+  const LogoutUser = () => {
+    dispatch(logout());
+    navigate("/");
+  };
+
+  return { NewUser, LoginUser, LogoutUser };
+};
--- frontend/src/routes/AppRouter.jsx
+++ frontend/src/routes/AppRouter.jsx
@@ -2,28 +2,18 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import { Home } from "../pages/Home/Home";
 import { useValidators } from "../hooks/useValidators";
 import { Toaster } from "react-hot-toast";
+import { Nav } from "../components/Nav/Nav";
+import { Login } from "../pages/Login/Login";
 
 export const AppRouter = () => {
   const { isUserAuthenticated } = useValidators();
 
   return (
     <>
+      <Nav />
       <Routes>
-        {/* Public routes */}
-        {!isUserAuthenticated() ? (
-          <>
-            <Route path="/" element={<Home />} />
-            {/* <Route path="/login-user" element={<Login />} />
-            <Route path="/register-user" element={<Register />} />
-            <Route path="*" element={<Navigate to="/login-user" />} /> */}
-          </>
-        ) : (
-          // Private routes
-          <Route>
-            {/* <Route path="/authLayout" element={<Home />} />
-            <Route path="*" element={<Navigate to="/" />} /> */}
-          </Route>
-        )}
+        <Route path="/" element={<Home />} />
+        <Route path="/login-user" element={<Login />} />
       </Routes>
       <Toaster position="top-center" reverseOrder={false} />
     </>
