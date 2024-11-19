5899a7f77b3809b643dc269ef30438eeb0ca3774
diego072117 <parracalderond9@gmail.com>
2024-11-19 15:46:01 -0500

    feat(gpt): implements api chat gpt.

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,11 +2,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import usersSlice from "./users/slice";
 import postsSlice from "./posts/slice"
+import gptSlice from "./gpt/slice"
 
 const store = configureStore({
   reducer: {
     users: usersSlice,
     posts: postsSlice,
+    gpt: gptSlice,
   },
 });
 
